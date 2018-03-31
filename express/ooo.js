c589c4f78f24b9f2a94ec1aecf3484fb6d13ceb3
104207314 <104207314@mail2.nccu.tw>
2018-03-31 06:50:25 -0400

    jsprob

--- express/ooo.js
+++ express/ooo.js
@@ -4,7 +4,7 @@ const app= express();
 
 app.get('/', function (req, res) {
   res.send('Hello !')
-});
+});//主頁輸出 "Hello"
 
 
 app.get('/test', function (req, res) {
@@ -19,10 +19,11 @@ app.get('/about/:name/:nickname', function(req, res) {
 
 
 app.use(express.bodyParser()); 
+
 app.post('/', function(req, res){
     console.log(request.body);
     res.send(req.body.name);
-});//POST
+});//POST, bodyParser是node.js 中间件，用于处理 JSON, Raw, Text 和 URL 编码的数据。
 
 app.listen(3000,function(){
   console.log (' app is running on port 3000!');});
