daa440fd3b740ddff3c2a1a20f4fbc9d623be861
104207314 <104207314@mail2.nccu.tw>
2018-04-12 12:23:04 -0400

    user

--- express/ooo.js
+++ express/ooo.js
@@ -6,17 +6,16 @@ app.use(express.static(__dirname+'/views'));
 app.set('view engine','ejs' );
 
 app.get('/about',function(req,res){
-   var user = {name: 'Tom'};
+  console.log(req.query);
   res.render('index',{name:req.query.name});
   
 });
 
-app.get('/name/:name', function(req, res){
-   var user = {name: 'Tom'};
-  res.send(req.parms.name);
-  
-
+app.get('/about/:name/:nickname', function(req, res) {
+    console.log(req.params)
+    res.send(req.params.name);
 });
+
 app.listen(3000,function(){
   console.log('Running on port 3000!');
 });
