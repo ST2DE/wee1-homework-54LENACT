9dea638df35e24a3d04adfc740ee57e99f7b6b07
104207314 <104207314@mail2.nccu.tw>
2018-04-14 09:59:24 -0400

    0001

--- express/ooo.js
+++ express/ooo.js
@@ -16,11 +16,7 @@ app.get('/about/:name/:nickname', function(req, res) {
     res.send(req.params.name);
 });
 
-app.get('/template', function(req, res){
-    var user = {name: 'Tom'}
-    res.render('index', {user: user});
-    // 不用寫 views/index.ejs 因為 express 預設 template會放在 views 資料夾裡面
-});
+
 
 
 app.listen(3000,function(){
